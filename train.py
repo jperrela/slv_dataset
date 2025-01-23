63cd5ca237763a836b5ac5d06dcbf33b351ccb77
jperrela <65288583+jperrela@users.noreply.github.com>
2025-01-23 21:28:32 +0100

    Update train.py
    changed epochs

--- train.py
+++ train.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 # Use the model
-results = model.train(data="data.yaml", epochs=50)  # train the model
+results = model.train(data="data.yaml", epochs=300)  # train the model
